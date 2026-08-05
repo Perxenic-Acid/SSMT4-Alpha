50c66197aafc5126c24b58011a902ec918e33812
Perxenic Acid <angeloyrd856@gmail.com>
2026-08-05 15:09:46 +0800

    添加贴图格式这一 metadata 于 theherta 对接信息. 理论上不影响希尔分支识别.

--- src-tauri/src/extract_new/efmi3.rs
+++ src-tauri/src/extract_new/efmi3.rs
@@ -867,8 +867,11 @@ impl EFMI3FullNewExtractor {
                 trianglelist_deduped_map.insert(
                     trianglelist_texture_file_name,
                     TrianglelistDedupedTextureProperty {
-                        fa_log_deduped_file_name: fa_log_deduped_filename,
+                        fa_log_deduped_file_name: fa_log_deduped_filename.clone(),
                         fa_data_deduped_file_name: fa_data_deduped_filename,
+                        format: SSMTStringUtils::extract_texture_format_from_deduped_file_name(
+                            &fa_log_deduped_filename,
+                        ),
                     },
                 );
             }
